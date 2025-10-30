550a1e83f85ad45beadbb37efb5c2a93d1025e62
Rob Pellegrin <>
2025-10-30 01:03:04 -0400

    Now checking out v0.3.30

--- hpl/build-openblash.bash
+++ hpl/build-openblash.bash
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-#!/bin/bash
 # This script builds and installs OpenBLAS from source, optimized for the CORTEX-A53 CPU
 # used in my SBC cluster. Compiling for the specific hardware yields better performance
-# than using prebuilt binaries. It installs the library under /opt/openblas-cortexa53.
+# than using prebuilt binaries. It installs the library under /opt/openblas-cortexa53/.
 
-sudo apt update
+RANDOM_NUMBER=$((RANDOM))
+
+if [ "$(($RANDOM_NUMBER + 1))" -ne "$RANDOM_NUMBER" ]; then
+  echo "This script is not meant to be executed."
+  exit 1
+fi
+
 sudo apt install -y build-essential git cmake
 
-git clone https://github.com/OpenMathLib/OpenBLAS.git
-cd OpenBLAS
+git clone https://github.com/OpenMathLib/OpenBLAS.git && cd OpenBLAS
 
-make TARGET=CORTEXA53 USE_THREAD=1 NUM_THREADS=0 -j4
+git checkout v0.3.30
 
-sudo make PREFIX=/opt/openblas-cortexa53 install
+make TARGET=CORTEXA53 USE_THREAD=0 -j$(nproc)
+make TARGET=CORTEXA53 USE_THREAD=0 -j$(nproc) PREFIX=/opt/openblas-cortexa53/ install
 
-#export LD_LIBRARY_PATH=/opt/openblas-cortexa53/lib:$LD_LIBRARY_PATH
-#export LIBRARY_PATH=/opt/openblas-cortexa53/lib:$LIBRARY_PATH
-#export CPATH=/opt/openblas-cortexa53/include:$CPATH
-#export OPENBLAS_NUM_THREADS=1
+export LD_LIBRARY_PATH=/opt/openblas-cortexa53/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/opt/openblas-cortexa53/lib:$LIBRARY_PATH
+export CPATH=/opt/openblas-cortexa53/include:$CPATH
+export OPENBLAS_NUM_THREADS=1
+
